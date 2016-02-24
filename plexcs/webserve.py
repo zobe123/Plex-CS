0f2ceb201135aff2768eb35cff558666156c7b14
zobe123 <manuel.zobl@gmx.at>
2016-02-24 23:44:04 +0100

    Update webserve.py

--- plexcs/webserve.py
+++ plexcs/webserve.py
@@ -1,4 +1,4 @@
-﻿# This file is part of Plex:CS.
+# This file is part of Plex:CS.
 #
 #  Plex:CS is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
